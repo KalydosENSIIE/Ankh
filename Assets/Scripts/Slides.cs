75b30f02fbd1641254653ded6acba1b181297441
Lucas_Brelivet <lucas.brelivet@laposte.net>
2023-02-05 16:47:45 +0100

    Changed Slide system

--- Assets/Scripts/Slides.cs
+++ Assets/Scripts/Slides.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class Slides : MonoBehaviour
 {
+    public List<SlideTransition> transitions;
     private int currentIndex;
 
     [SerializeField] private Fader fader;
@@ -26,16 +27,38 @@ public class Slides : MonoBehaviour
 
     private IEnumerator Transition()
     {
-        fader.FadeOut();
-        yield return new WaitForSeconds(fader.fadeOutTime);
-        transform.GetChild(currentIndex).gameObject.SetActive(false);
-        currentIndex ++;
-        if (currentIndex < transform.childCount)
-            transform.GetChild(currentIndex).gameObject.SetActive(true);
+        if(currentIndex < transitions.Count)
+        {
+            if(transitions[currentIndex].fade)
+            {
+                fader.FadeOut();
+                yield return new WaitForSeconds(fader.fadeOutTime);
+            }
+            foreach(GameObject obj in transitions[currentIndex].disable)
+            {
+                obj.SetActive(false);
+            }
+            foreach(GameObject obj in transitions[currentIndex].enable)
+            {
+                obj.SetActive(true);
+            }
+            if(transitions[currentIndex].fade)
+            {
+                fader.FadeIn();
+            }
+        }
         else
         {
-            SceneManager.LoadScene(nextSceneIndex);
+            fader.TransitionToScene(nextSceneIndex);
         }
-        fader.FadeIn();
+        currentIndex ++;
     }
+}
+
+[System.Serializable]
+public struct SlideTransition
+{
+    public bool fade;
+    public List<GameObject> disable;
+    public List<GameObject> enable;
 }
